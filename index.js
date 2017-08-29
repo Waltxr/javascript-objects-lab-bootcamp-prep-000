5c107cb9d570cf49d2004b6756c82a33b90019a9
Adam Walter <adam.iverson.walter@gmail.com>
2017-08-29 17:29:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-   object[key] = value
-   return object
+  object[key] = value
+  return object
 }
 
 function deleteFromObjectByKey(object, key) {
-   var newObj = Object.assign({}, object)
-   delete newObj[key]
-   return newObj
+  var newObj = Object.assign({}, object)
+  delete newObj[key]
+  return newObj
 }
 
 function destructivelyDeleteFromObjectByKey() {
